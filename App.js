ae2fa451f2097f805e619ea2c1d446305242ee53
Michelle Bautista <alejandra.lopez.is@unipolidgo.edu.mx>
2022-07-08 08:02:25 -0500

    notificacion

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
-import * as React from "react";
-import { Text, View } from "react-native";
+import React, { useState, useEffect, useRef } from 'react';
+import { Text, View, Platform  } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { SafeAreaProvider } from "react-native-safe-area-context";
@@ -8,6 +8,8 @@ import InicioSesion from "./IniciarSesion";
 import Subir from "./Subir";
 import Login from "./Login";
 import { createUserWithEmailAndPassword, signInWithEmailAndPassword, getAuth, onAuthStateChanged, signOut } from "firebase/auth";
+import * as Device from 'expo-device';
+import * as Notifications from 'expo-notifications';
 
 function HomeScreen() {
   return (
@@ -26,16 +28,57 @@ function SettingsScreen() {
 }
 
 const Tab = createBottomTabNavigator();
+const registerForPushNotificationsAsync = async () => {
+  if (Device.isDevice) {
+    const { status: existingStatus } = await Notifications.getPermissionsAsync();
+    let finalStatus = existingStatus;
+    if (existingStatus !== 'granted') {
+      const { status } = await Notifications.requestPermissionsAsync();
+      finalStatus = status;
+    } else {
+      console.log (finalStatus);
+    }
+    if (finalStatus !== 'granted') {
+      alert('Failed to get push token for push notification!');
+      return;
+    }
+    const token = (await Notifications.getExpoPushTokenAsync()).data;
+    console.log(token);
+    alert(token);
+    this.setState({ expoPushToken: token });
+  } else {
+    alert('Must use physical device for Push Notifications');
+  }
+
+  if (Platform.OS === 'android') {
+    Notifications.setNotificationChannelAsync('default', {
+      name: 'default',
+      importance: Notifications.AndroidImportance.MAX,
+      vibrationPattern: [0, 250, 250, 250],
+      lightColor: '#FF231F7C',
+    });
+  }
+  };
 
 export default function App() {
   const auth = getAuth();
+  useEffect(() => {
+    registerForPushNotificationsAsync()
+  }, [])
+  
   return (
     <SafeAreaProvider>
       <NavigationContainer>
         <Tab.Navigator>
           
+          <Tab.Screen name="Home" component={HomeScreen} />
+          <Tab.Screen name="Users"component={UserStack} options={{ headerShown: false }}/>
           <Tab.Screen name="Aplicación" component={UserStack} />
-         
+          
+          <Tab.Screen name="Settings" component={SettingsScreen} />
+          <Tab.Screen name="Subir" component={Subir} />
+           <Tab.Screen name="Subir Imagen" component={Subir} />
+          
           
         </Tab.Navigator>
       </NavigationContainer>
